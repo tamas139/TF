6b69455f86f1049c3df57ece6c1f555b09473304
BuildTools <unconfigured@null.spigotmc.org>
2020-06-09 22:26:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  filter {
+  # filter {
     name   = "test"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
+  # }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  }
-
+  } 
+  
   owners = ["545947116725"] # Canonical
 }
 
