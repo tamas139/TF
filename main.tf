f463395c291412bfa15a96388499022828d69ac5
BuildTools <unconfigured@null.spigotmc.org>
2020-06-09 22:32:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
   region = "us-west-2"
 }
@@ -5,17 +7,17 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  # filter {
-    name   = "test"
+  filter {
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  # }
+  }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  } 
-  
-  owners = ["545947116725"] # Canonical
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
@@ -23,6 +25,6 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "NokiaJobApply"
+    Name = "HelloWorld"
   }
 }
